f142a3877df12e033902a1f375cfd17546c07190
Jorge85015 <jorge85015@gmail.com>
2024-11-26 13:41:19 +0100

    feature/alphabeta-file-maybe-finished

--- alphabeta.py
+++ alphabeta.py
@@ -1,4 +1,3 @@
-
 # AlphaBeta Partial Search
 
 infinity = 1.0e400
@@ -7,19 +6,39 @@ infinity = 1.0e400
 def terminal_test(state, depth):
     return depth <= 0 or state.is_terminal
 
+
 def max_value(state, player, max_depth, alpha, beta, eval_function):
-    """
-    Completar con el codigo correspondiente a la funcion <max_value> de la
-    version del algoritmo minimax con poda alfa-beta
-    """
-    return 0
+    if terminal_test(state, max_depth):
+        return eval_function(state, player)
+    value = -infinity
+    for action, successor in state.successors():
+        value = max(
+            value,
+            min_value(successor, player, max_depth - 1, alpha, beta, eval_function),
+        )
+        if value >= beta:
+            return value
+        alpha = max(alpha, value)
+    return value
+
 
 def min_value(state, player, max_depth, alpha, beta, eval_function):
     """
     Completar con el codigo correspondiente a la funcion <min_value> de la
     version del algoritmo minimax con poda alfa-beta
     """
-    return 0
+    if terminal_test(state, max_depth):
+        return eval_function(state, player)
+    value = infinity
+    for action, succesor in state.successors():
+        value = min(
+            value,
+            max_value(succesor, player, max_depth - 1, alpha, beta, eval_function),
+        )
+        if value <= alpha:
+            return value
+        beta = min(beta, value)
+    return value
 
 
 def alphabeta_search(game, max_depth, eval_function):
@@ -33,9 +52,9 @@ def alphabeta_search(game, max_depth, eval_function):
     # Searches for the action leading to the sucessor state with the highest min score
     successors = game.successors()
     best_score, best_action = -infinity, successors[0][0]
-    for (action, state) in successors:
+    for action, state in successors:
         score = min_value(state, player, max_depth, -infinity, infinity, eval_function)
         if score > best_score:
             best_score, best_action = score, action
-    
+
     return best_action
